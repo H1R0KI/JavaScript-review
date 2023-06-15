a98000ccc6ab4df90accd2f8142e86395cbcc8b8
H1R0KI <122168203+H1R0KI@users.noreply.github.com>
2023-06-15 13:36:17 +0000

    [Finish]変数宣言

---
+++ sample1.js
@@ -0,0 +1,6 @@
+//再宣言は"var"のみ可能  let,constは不可
+
+var nickname = "taro"
+console.log(nickname)
+var nickname = "ichiro"
+console.log(nickname)
---
+++ sample2.js
@@ -0,0 +1,5 @@
+// varによる再代入 var,letは再代入可能 constは不可
+var nickname = "taro"
+console.log(nickname)
+nickname = "jiro"
+console.log(nickname)
